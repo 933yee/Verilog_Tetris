cea7c0b35c9e7ffc05c3254dec79d820f9a5a473
933yee <kevins30102@yahoo.com.tw>
2023-01-06 14:29:36 +0800

    add tetris_final_version

---
+++ tetris_final_version/clock_divisor_0_0_5s.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+module clock_divisor_0_0_5s (clk, clk_out, rst);
+    output reg clk_out = 1'b0;
+    input clk, rst;
+    reg [31:0]count = 32'd0;
+    always@(posedge clk) begin
+        if(rst) begin
+            clk_out <= 0;
+            count <= 0;
+        end else if(count == 32'd5000000) begin
+            count <= 32'd0;
+            clk_out <= 1;
+        end
+        else begin
+            clk_out <= 0;
+            count <= count + 1'b1;
+        end
+    end
+endmodule
